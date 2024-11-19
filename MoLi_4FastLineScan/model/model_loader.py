4dc84a4c656996f7c65acf46f46ae7dca72fef1e
Yurong <43307048+YurongChen1998@users.noreply.github.com>
2024-11-19 08:36:54 +0800

    Add code of MoLi for fast linescan hyperspectral imaging (2024-11-19)

---
+++ MoLi_4FastLineScan/model/model_loader.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import os
+from collections import OrderedDict
+from model.LRNet import Deep_Image_Prior_Network
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+def low_rank_model_load(rank):
+    im_net = Deep_Image_Prior_Network(rank, 'reflection',
+                            upsample_mode=['nearest', 'nearest', 'bilinear'],
+                            skip_n33d=32,
+                            skip_n33u=32,
+                            skip_n11=12,
+                            num_scales=3,
+                            n_channels=119)                                             
+    return [im_net.to(device)]
+
